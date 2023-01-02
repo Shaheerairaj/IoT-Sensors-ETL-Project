681f873d1e625ea3a62b4a03fb443461842816b8
Shaheer Airaj Ahmed <shaheerairajahmed@gmail.com>
2023-01-02 22:10:38 +0400

    Added notes for next day

--- script2-monnit-api-call.py
+++ script2-monnit-api-call.py
@@ -1,6 +1,9 @@
 import pandas as pd
 import requests
 import datetime
+import logging
+
+
 
 sensor_list_URL = 'https://www.imonnit.com/json/SensorList'
 data_message_URL= 'https://www.imonnit.com/json/SensorDataMessages'
@@ -11,26 +14,24 @@ headers = {
 }
 
 r = requests.post(sensor_list_URL, headers=headers, verify=False)
-
 data = r.json()
 
-print(data['Result'])
+
+
 
 sensor_list = []
+sensor_name = []
 
-for i in range(0, len(data['Result'])):
+for sensorID in range(0, len(data['Result'])):
 
-    sensor_list.append(data['Result'][i]['SensorID'])
+    sensor_list.append(data['Result'][sensorID]['SensorID'])
+    sensor_name.append(data['Result'][sensorID]['SensorName'])
 
-print(sensor_list)
 
 
 
 fromDate = (datetime.datetime.now() - datetime.timedelta(hours=6)).strftime("%m/%d/%Y %H:00:00")
 toDate   = (datetime.datetime.now() - datetime.timedelta(hours=5)).strftime("%m/%d/%Y %H:00:00")
-
-print(fromDate)
-print(toDate)
 
 data_message_params = {
     'sensorID':sensor_list[0],
@@ -40,10 +41,14 @@ data_message_params = {
 
 r = requests.post(data_message_URL, params=data_message_params, headers=headers, verify=False)
 data = r.json()
-print(data)
 
 df = pd.DataFrame(data['Result'], index=[0])
 df['MessageDate'] = int(df['MessageDate'].str[6:-2]) / 1000
 df['MessageDate'] = (datetime.datetime.fromtimestamp(df['MessageDate'])).strftime("%Y-%m-%d %H:%M:%S")
 
 print(df.columns)
+
+# Complete logging
+# Add sensor name to df
+# Exception handling when calling API
+# For loop to call data for all sensors
