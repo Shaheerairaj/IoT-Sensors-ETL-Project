a3010dd0ec484cfa1eda26e924a21707a7fa7f66
Shaheer Airaj Ahmed <shaheerairajahmed@gmail.com>
2023-01-06 14:22:21 +0400

    Commit before 1st try

--- script3-sending-data-to-database.py
+++ script3-sending-data-to-database.py
@@ -1,4 +1,8 @@
 import mysql.connector
+import pandas as pd
+
+df = pd.read_excel('Sensor data.xlsx')
+df = df.iloc[0:5]
 
 # Connect to MySQL server and run a query (create a test database)
 
@@ -10,4 +14,10 @@ db = mysql.connector.connect(
 
 mycursor = db.cursor()
 
-mycursor.execute("CREATE DATABASE testdatabase")
+# Creating column list for insertion
+cols = "`,`".join([str(i) for i in df.columns.tolist()])
+
+# Inserting DataFrame records
+for i, row in df.iterrows():
+    sql = "INSERT INTO `vibration_sensor` (`" + cols +"`) VALUES (" + "%s,"*(len(row)-1) + "%s)"
+    mycursor.execute(sql, tuple(row))
