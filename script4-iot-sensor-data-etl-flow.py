fc7ce06852966ef7556ceaa8668c4896d75cd94c
Shaheer Airaj Ahmed <shaheerairajahmed@gmail.com>
2023-01-06 17:09:30 +0400

    Completed full etl script

--- script4-iot-sensor-data-etl-flow.py
+++ script4-iot-sensor-data-etl-flow.py
@@ -1,12 +1,13 @@
 import pandas as pd
+import mysql.connector
 import requests
 import datetime
 import logging
 import warnings
 warnings.filterwarnings("ignore")
 
-logging.basicConfig(filename='script2-monnit-api-call.log', level=logging.INFO,
-format='%(asctime)s:%(levelname)s:%(message)s')
+# logging.basicConfig(filename='etl.log', level=logging.INFO,
+# format='%(asctime)s:%(levelname)s:%(message)s')
 
 scriptStartTime = datetime.datetime.now()
 
@@ -89,9 +90,61 @@ df['SensorType'] = new[0]
 
 
 
+
+
+
+
+
+sensor_tables = {
+    'Advanced Vibration':'vibration',
+    'AirSpeed':'air_speed',
+    'Air Quality':'air_quality',
+    'CO2 Meter':'co2',
+    'CO Meter':'co',
+    'Differential Pressure':'diff_press',
+    'Duct Temperature':'duct_temp',
+    'Humidity':'humidity',
+    'LightSensor':'light',
+    'PIR ALTA':'motion',
+    'Quad Temperature':'quad_temp',
+    'Temperature':'temp'
+}
+
+# Connect to MySQL server and run a query (create a test database)
+logging.info("Connecting to database")
+db = mysql.connector.connect(
+    host='localhost',
+    user='root',
+    passwd='QwertyLayout',
+    db='monnit_iot_sensors'
+)
+
+mycursor = db.cursor()
+
+# Creating column list for insertion
+cols = "`,`".join([str(i) for i in df.columns.tolist()])
+
+# Filtering df fr sensor type
+logging.info("Ïnserting data to database")
+for sensorType in df['SensorType'].unique():
+
+    df_filtered = df[df['SensorType'] == sensorType]
+    dataMessage = df_filtered['DataMessageGUID']
+
+    try:
+        # Inserting DataFrame records
+        for i, row in df_filtered.iterrows():
+            sql = "INSERT INTO `"+ sensor_tables[sensorType] + "` (`" + cols +"`) VALUES (" + "%s,"*(len(row)-1) + "%s)"
+            mycursor.execute(sql, tuple(row))
+
+            db.commit()
+
+    except:
+        logging.warning(f"Error faced for table {sensor_tables[sensorType]} for data message: {dataMessage}")
+
+db.close()
+
+
 scriptEndTime = datetime.datetime.now()
 scriptRunningTime = scriptEndTime - scriptStartTime
 logging.info(f"Time taken to run script: {scriptRunningTime}")
-
-
-df.to_excel('Sensor data.xlsx', index=False)
